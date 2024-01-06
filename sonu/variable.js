f60f0227e740a0359989d6fce002fcaef417c00f
codewithMe76 <155834365+codewithMe76@users.noreply.github.com>
2024-01-06 17:16:42 +0000

    config done

--- sonu/variable.js
+++ sonu/variable.js
@@ -4,7 +4,7 @@ var accountPassowrd=12389
 accountCity="jaipur"
 /*
 Note:- Prefer not to use var beacuse 
-block scope and functional scope
+block scope and functional scope .
 */
 console.log(accountId);
 console.table([accountId,accountEmail,accountPassowrd,accountCity])
