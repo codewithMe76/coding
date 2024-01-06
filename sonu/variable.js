e78e08499d045c2059339ffb9cf08bb965ce6f35
codewithMe76 <155834365+codewithMe76@users.noreply.github.com>
2024-01-06 17:12:02 +0000

    config done

---
+++ sonu/variable.js
@@ -0,0 +1,10 @@
+const accountId=2334455
+let accountEmail="Sonu@google.cp"
+var accountPassowrd=12389
+accountCity="jaipur"
+/*
+Note:- Prefer not to use var beacuse 
+block scope and functional scope
+*/
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassowrd,accountCity])
